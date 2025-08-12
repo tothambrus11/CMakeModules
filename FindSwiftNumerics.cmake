08ced7b8be5a232ade4ee19bb7ac4749a01178ae
Ambrus Toth <ping@ambrus.dev>
2025-08-12 23:37:27 +0200

    Fix patch

--- FindSwiftNumerics.cmake
+++ FindSwiftNumerics.cmake
@@ -1,10 +1,10 @@
 include(./FetchHyloDependency)
 
-set(patch_swift_numerics
-  ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_LIST_DIR}/scripts/GitPatch.cmake
-  ${CMAKE_CURRENT_LIST_DIR}/patches/swift-numerics.patch)
+# set(patch_swift_numerics
+#   ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_LIST_DIR}/scripts/GitPatch.cmake
+#   ${CMAKE_CURRENT_LIST_DIR}/patches/swift-numerics.patch)
 
 fetch_hylo_dependency(SwiftNumerics
-  GIT_REPOSITORY https://github.com/apple/swift-numerics.git
-  GIT_TAG        1.0.3
+  GIT_REPOSITORY https://github.com/tothambrus11/swift-numerics.git
+  GIT_TAG        49b753eff41e5a2e2bf6d4aab9bccebb7e45a65e
 )
