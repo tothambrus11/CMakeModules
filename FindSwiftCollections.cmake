87e7c4dba4ad0bb06db9a6395bcd480ddb4a4d4d
Ambrus Toth <ping@ambrus.dev>
2025-08-13 22:03:44 +0200

    last one

--- FindSwiftCollections.cmake
+++ FindSwiftCollections.cmake
@@ -6,5 +6,5 @@ set(patch_swift_collections
 
 fetch_hylo_dependency(SwiftCollections
   GIT_REPOSITORY https://github.com/tothambrus11/swift-collections.git
-  GIT_TAG        e3e1fbbcac568ea141c851b70c81978371afdb8f
+  GIT_TAG        b2a87ac9987e63caef1c922c177f07b787b11104
 )
